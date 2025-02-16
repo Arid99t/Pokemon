66b360871b40386c120525ea5dbe91ef98fe277d
Arindam Dutta <arindam.dutta99999@gmail.com>
2025-02-16 06:40:49 +0000

    Battle Loop

---
+++ Pokemon/BattleManager.hpp
@@ -0,0 +1,15 @@
+#pragma once
+// BattleManager.hpp
+
+#include "Pokemon.hpp"
+#include "Player.hpp"
+
+
+class Pokemon;
+class BattleManager {
+public:
+    void startBattle(Player& player, Pokemon& wildPokemon);
+private:
+    void battle(Pokemon& playerPokemon, Pokemon& wildPokemon);
+    void handleBattleOutcome(Player& player, bool playerWon);
+};
